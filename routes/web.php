33fde2ab0047a1ecf6880f49470ebf450fa23461
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2025-02-04 15:10:37 +0300

    create factory and seed

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    // sleep(1);
-    return Inertia::render('Home', ['name' => 'Tolosa']);
-});
+Route::get('/', [PostController::class, 'index']);
+Route::resource('posts', PostController::class)->except('index');
+// Route::get('/', function () {
+//     // sleep(1);
+//     return Inertia::render('Home', ['name' => 'Tolosa']);
+// });
 // Route::inertia('/', 'Home'); 
