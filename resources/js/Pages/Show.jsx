f213f99f55fcac9e46e34b55ee1f9a7bdcef3de3
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2025-02-05 01:18:58 +0300

    delet

--- resources/js/Pages/Show.jsx
+++ resources/js/Pages/Show.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
-
+import { useForm } from '@inertiajs/react'
+import { useRoute } from '../../../vendor/tightenco/ziggy';
 function Show({post}) {
+    const { delete: destory } = useForm();
+    const route = useRoute();
+    function submit(e) {
+        e.preventDefault();
+        // destory(`/posts/${post.id}`);
+        destory(route("posts.destroy", post));
+    }
 return (
     <>
         <div className="mt-10 p-4 border-b">
@@ -9,7 +17,12 @@ return (
                         <span className="text-green-950">{new Date(post.created_at).toLocaleTimeString() }</span>
                 </div>
                 <p className="font-medium">{post.body}</p>
-                <button className='text-blue-500' onClick={() => window.location.href = '/'}>Back</button>
+                <div className="flex items-center justify-end gap-2">
+                    <form action="" onSubmit={submit}>
+                        <button type='submit' className='bg-red-500 rounded-md text-sm px-4 py-2 text-white'>Delete</button>
+                        <button className='bg-green-500 rounded-md text-sm px-4 py-2 text-black ml-2' onClick={() => window.location.href = '/'}>Back</button>
+                    </form>
+                </div>
         </div>
     </>
 )
